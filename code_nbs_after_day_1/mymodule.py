f97ee4b227070a631cfc22882afade09e3cd64e3
Steve Holden <steve@holdenweb.com>
2014-12-09 16:00:43 +0000

    Remove temporary directories after integration of updated content.

--- code_nbs_after_day_1/mymodule.py
+++
@@ -1,6 +0,0 @@
-
-def f(x):
-    print("x is:", x)
-    return x+42
-
-print("Loaded")
--- code_nbs_after_day_3/widget.py
+++
@@ -1,9 +0,0 @@
-#
-# Now with size() method that does nothing
-#
-class Widget:
-    def __init__(self, title, size=(50, 50)):
-        self.title = title
-        self._size = size
-    def size(self):
-        return self._size
